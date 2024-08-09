fe5af36f58c90b2cd0173bdd262ceaf84654f27b
Sebastian Manzano <samanzanom@gmail.com>
2024-08-09 20:11:38 +0100

    mejora cors

--- src/main/java/cl/equifax/apirest/config/SecurityConfig.java
+++ src/main/java/cl/equifax/apirest/config/SecurityConfig.java
@@ -50,7 +50,7 @@ public class SecurityConfig {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOrigins(List.of("*"));  // Permite todos los orígenes
+        configuration.setAllowedOriginPatterns(List.of("*")); //No recomendado para producción
         configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"));
         configuration.setAllowedHeaders(Arrays.asList("Authorization", "Content-Type", "X-Requested-With"));
         configuration.setExposedHeaders(List.of("Authorization"));
